3e0c993b46adf80162df5b1c00fe7aa2786f7e78
Connor Lagana <connor.lagana@gmail.com>
2020-10-01 11:15:29 -0400

    Initial Commit

---
+++ RedditRemake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RedditRemake
+//
+//  Created by Connor Lagana on 10/1/20.
+//  Copyright © 2020 Connor Lagana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
